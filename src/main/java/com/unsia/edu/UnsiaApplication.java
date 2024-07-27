117831b2a45a2180aac19d24ba774314eec0ec1b
AriSu2904 <arisusanto290401@gmail.com>
2024-07-27 22:32:22 +0700

    feat(unsia): Init Project

---
+++ src/main/java/com/unsia/edu/UnsiaApplication.java
@@ -0,0 +1,13 @@
+package com.unsia.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unsia/edu/UnsiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsia.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
